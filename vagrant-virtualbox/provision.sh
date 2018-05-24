1e3c65cc2e8a12db82c41bf19fb43feee2d65753
Phil Evans <pevans@cafex.com>
2018-05-24 15:38:37 +0100

    Added a gitignore file for sensitive information.

--- vagrant-virtualbox/provision.sh
+++ vagrant-virtualbox/provision.sh
@@ -27,4 +27,16 @@ sudo cp /vagrant/VirtualHost/jenkins /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/jenkins /etc/nginx/sites-enabled/
 sudo service nginx restart
 sudo service jenkins restart
+########################
+# Install docker
+########################
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+########################
+# Show initial password
+########################
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 echo "Success"
